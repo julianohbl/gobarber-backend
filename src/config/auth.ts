07cee9dece3e82bfe009d01fe82cafbea98c935d
julianohbl <julianohbl@hotmail.com>
2020-11-05 17:40:35 -0300

    Validating credentials, generating JWT token and authenticating routes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a935ca8aaa2b9d3c2b61a3270554c84d',
+    expiresIn: '1d',
+  },
+};
--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
   try {
-    return response.status(201).json({ ok: true });
+    const { email, password } = request.body;
+
+    const authenticateUserService = new AuthenticateUserService();
+
+    const { user, token } = await authenticateUserService.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.status(201).json({ user, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
